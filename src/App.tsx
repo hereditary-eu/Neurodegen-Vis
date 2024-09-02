5baa86b7b7ab7066d60ab49ea1141398971271b6
Heissenbaer <grabner.m9@gmail.com>
2024-09-02 11:44:01 +0200

    Tutorial_data_loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import * as d3 from "d3";
+
+function Michi() {
+  let x: number = 5;
+  const [data, setData] = useState(["leer"]);
+  useEffect(() => {
+    async function load() {
+      let loaded = (await d3.csv("/PD_SampleData_Curated.csv")).map((d) => d);
+      setData(["hallo", "welt", "wie", "gehts"]);
+      console.log(loaded);
+    }
+    load();
+  }, []);
+  return (
+    <>
+      <h2>Welcome! x = {x}</h2>
+      <div>Daten: {data[0]}</div>
+    </>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +48,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Michi />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
