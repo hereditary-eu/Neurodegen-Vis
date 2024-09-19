1b03c3b5227533b36e3c5f54535f41798942cf0e
Heissenbaer <grabner.m9@gmail.com>
2024-09-19 11:00:16 +0200

    scatterplot with label select added

--- src/App.tsx
+++ src/App.tsx
@@ -39,12 +39,27 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
 
   // Obsverable plot
 
-  const rand_histo = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
+  // const rand_histo = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
+  const dur_histo = Plot.plot({
+    marks: [
+      // Plot.rectY(patients_age, Plot.binX({y: "count"}, {x: d => d})),
+      Plot.rectY(patients_data, Plot.binX({y: "count", thresholds: 8}, {x: "npsid_ddur_v"})),
+      Plot.ruleY([0]),
+      // Plot.ruleX([0])
+    ],
+    x: {
+      label: "Age",
+      tickFormat: (d: number) => d.toString(),
+    },
+    y: {
+      label: "Frequency",
+    },
+  });
   const div = document.querySelector("#myplot");
   if (div) {
     console.log("div found");
     div.innerHTML = ''; // Clear the div
-    div.appendChild(rand_histo);
+    div.appendChild(dur_histo);
   }
   else {
     console.log("div not found");
@@ -189,15 +204,16 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
       </div>
       <div className="flex-container">
         <div>
-          <p>Scatterplot Obsverable</p>
+          <p>Age vs attention z-score</p>
           <div ref={scatterplot_ref}></div>
         </div>
-      </div>
-      <div className="flex-container">
         <div>
           <p>Duration PD vs z-score</p>
           <div ref={pd_duration_ref}></div>
         </div>
+      </div>
+      <div className="flex-container">
+
       </div>
       <div className="flex-container">
       <div>
@@ -213,15 +229,74 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
   );
 }
 
-function scatterplot(feature: string, patients_data: Patient[]) {
-  return "";
+interface ScatterplotProps {
+  feature: string;
+  patients_data: Patient[];
 }
 
+
+function linReg(xArray: number[], yArray: number[]): [number, number] {
+  // Calculate Sums
+  let xSum=0, ySum=0 , xxSum=0, xySum=0;
+  let count = xArray.length;
+
+  for (let i = 0, len = count; i < count; i++) {
+    xSum += xArray[i];
+    ySum += yArray[i];
+    xxSum += xArray[i] * xArray[i];
+    xySum += xArray[i] * yArray[i];
+  }
+
+  let slope = (count * xySum - xSum * ySum) / (count * xxSum - xSum * xSum);
+  let intercept = (ySum / count) - (slope * xSum) / count;
+  
+  return [slope, intercept];
+}
+
+
+function PlotScatterplot({feature, patients_data}: ScatterplotProps) {
+  console.log("Scatterplot fun started");
+  console.log(patients_data);
+
+  const pd_duration = Plot.plot({
+    marks: [
+      Plot.dot(patients_data, {x: "npsid_ddur_v", y: feature}),
+      // Plot.ruleX([Math.min(...patients_age.filter((d) => !isNaN(d)))-1]),
+      Plot.ruleY([-1]),
+    ],
+    x: {
+      label: "Duration PD",
+      // tickFormat: (d: number) => d.toString(),
+    },
+    y: {
+      label: feature,
+    },
+  });
+
+  const pd_duration_ref = useRef<HTMLDivElement>(null);  // Create a ref to access the div element
+  if (pd_duration_ref.current) {
+    pd_duration_ref.current.innerHTML = ''; // Clear the div
+    pd_duration_ref.current.appendChild(pd_duration);
+    // histogramRef.current.appendChild(rand_histo);
+  }
+
+  return (
+  <div className="flex-container">
+    <div>
+      <p> Scatterplot {feature}</p>
+      <div ref={pd_duration_ref}></div>
+    </div>
+  </div>
+  );
+}
 
 function App() {
   const [count, setCount] = useState<number>(0);
   const [feature, setFeature] = useState<string>("insnpsi_age");
-  const feature_list: string[] = ["insnpsi_age", "attent_z_comp", "npsid_ddur_v", "attent_sum_z", "attent_sum"];
+  const z_score_features: string[] = ["attent_z_comp", "exec_z_comp", "visuosp_z_comp", 
+    "memory_z_comp", "language_z_comp"];
+  const feature_list: string[] = ["insnpsi_age", "attent_z_comp", "npsid_ddur_v", "attent_sum_z"
+    , "attent_sum"];
 
   let emptyPatient: Patient = new Patient();
   const [patients_data, setData] = useState(Array(54).fill(emptyPatient)); // todo, hard coded 54
@@ -263,8 +338,9 @@ function App() {
       <Michi count={count} patients_data={patients_data} selected_feature=""/>
       <label htmlFor="feature">Choose a feature:  </label>
       <select name="feature" id="feature" onChange={(e) => setFeature(e.target.value)}>
-        {feature_list.map((f) => <option value={f}>{f}</option>)}
+        {z_score_features.map((f) => <option value={f}>{f}</option>)}
       </select>
+      <PlotScatterplot feature={feature} patients_data={patients_data}/>
     </>
   );
 }
