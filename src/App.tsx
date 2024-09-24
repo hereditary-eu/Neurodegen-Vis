374bf690f4d397bbd27ca5d8014c71104d9f3286
Heissenbaer <grabner.m9@gmail.com>
2024-09-24 11:12:56 +0200

    second linReg plot, code cleanup minMax function

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,7 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
   console.log("Michi fun started");
   let binNumber = 8;
 
-  console.log(patients_data);
-  console.log(patients_data[0].record_id);
-  console.log(patients_data.length);
-  console.log("Michi patients data read");
-
   let total_age: number = 0;
-  // patients_data.forEach((p) => {console.log(p.insnpsi_age);});
   patients_data.forEach((p) => {total_age += p.insnpsi_age;});
 
   let patients_age: number[] = [];
@@ -32,7 +26,6 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
 
   let patients_attent_z_comp: number[] = [];
   patients_data.forEach((p) => {patients_attent_z_comp.push(p.attent_z_comp);});
-  // console.log(patients_attent_z_comp);
 
   // ----------------------------- scatterplot -----------------------------
   console.log("d3 image created");
@@ -57,7 +50,6 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
   });
   const div = document.querySelector("#myplot");
   if (div) {
-    console.log("div found");
     div.innerHTML = ''; // Clear the div
     div.appendChild(dur_histo);
   }
@@ -178,12 +170,6 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
   }
 
   console.log("obs plot created");
-  // console.log(plot);
-  // console.log(age_histo);
-  // console.log(div);
-  // console.log(svg)
-  // div.append(histogram);
-  // console.log(div)
   return (
     <>
       <div className="flex-container">
@@ -223,17 +209,9 @@ function Michi({count, patients_data, selected_feature}: MichiProps) {
   );
 }
 
-interface ScatterplotProps {
-  feature: string;
-  patients_data: Patient[];
-}
-
-
 function linReg(xArray: number[], yArray: number[]): [number, number] {
   // Calculate Sums
   console.log("linReg fun started");
-  console.log(xArray);
-  console.log(yArray);
   let xSum=0, ySum=0 , xxSum=0, xySum=0;
   let count = xArray.length;
 
@@ -259,36 +237,50 @@ function linReg(xArray: number[], yArray: number[]): [number, number] {
 }
 
 
-function PlotScatterplot({feature, patients_data}: ScatterplotProps) {
+interface ScatterplotProps {
+  y_feature: string;
+  x_feature: string;
+  patients_data: Patient[];
+}
+
+function calcMinMax({y_feature, x_feature, patients_data}: ScatterplotProps){
+  const min_x = Math.min(...patients_data.map((p) => p[x_feature]).filter((d) => !isNaN(d)));
+  const max_x = Math.max(...patients_data.map((p) => p[x_feature]).filter((d) => !isNaN(d)));
+  const min_y = Math.min(...patients_data.map((p) => p[y_feature]).filter((d) => !isNaN(d)));
+  const max_y = Math.max(...patients_data.map((p) => p[y_feature]).filter((d) => !isNaN(d)));
+  const x_range = max_x - min_x;
+  const y_range = max_y - min_y;
+  return [min_x - x_range*0.05, max_x + x_range*0.05, min_y - x_range*0.05, max_y + y_range*0.05];
+}
+
+function PlotScatterplot({y_feature, x_feature, patients_data}: ScatterplotProps) {
   console.log("Scatterplot fun started");
-  console.log(patients_data);
+
+  let [min_x, max_x, min_y, max_y] = calcMinMax({y_feature, x_feature, patients_data});
+  console.log("--min max--");
+  console.log(min_x);
+  console.log(max_x);
+  console.log(min_y);
+  console.log(max_y);
 
   let slope: number = 0;
   let intercept: number = 0;
-  [slope, intercept] = linReg(patients_data.map((p) => p.npsid_ddur_v), patients_data.map((p) => p[feature]));
-  console.log(slope);
-  console.log(intercept);
+  [slope, intercept] = linReg(patients_data.map((p) => p[x_feature]), patients_data.map((p) => p[y_feature]));
 
-  // const linReg_x = [Math.min(...patients_data.map((p) => p.npsid_ddur_v)),
-  //    Math.max(...patients_data.map((p) => p.npsid_ddur_v))];
-  const linReg_x = [0, 10, 25];
+  const linReg_x = [min_x, max_x];
   const linReg_y = linReg_x.map((x) => slope * x + intercept);
-  console.log(linReg_x);
-  console.log(linReg_y);
   const linRegData = linReg_x.map((x, i) => ({x: x, y: linReg_y[i]}));
-  // throw TypeError("test");
-
   const pd_duration = Plot.plot({
     marks: [
-      Plot.dot(patients_data, { x: "npsid_ddur_v", y: feature, tip: true }),
+      Plot.dot(patients_data, { x: x_feature, y: y_feature, tip: true }),
       Plot.line(linRegData, {x: "x", y: "y", stroke: "blue"}),
-      // Plot.crosshair(olympians, {x: "weight", y: "height"})
+      Plot.crosshair(patients_data, { x: x_feature, y: y_feature, tip: true }),
     ],
     x: {
-      label: "Duration PD",
+      label: x_feature,
     },
     y: {
-      label: feature,
+      label: y_feature,
     },
     style: "--plot-background: black; "
   });
@@ -301,27 +293,27 @@ function PlotScatterplot({feature, patients_data}: ScatterplotProps) {
   }
 
   return (
-  <div className="flex-container">
-    <div>
-      <p> Scatterplot {feature}, slope={slope}</p>
-      <div ref={pd_duration_ref}></div>
+    <>
+    <div className="flex-container">
+      <div>
+        <p> Scatterplot {y_feature}, slope={slope}</p>
+        <div ref={pd_duration_ref}></div>
+      </div>
     </div>
-  </div>
+    </>
   );
 }
 
 function App() {
   const [count, setCount] = useState<number>(0);
-  const [feature, setFeature] = useState<string>("insnpsi_age");
   const z_score_features: string[] = ["attent_z_comp", "exec_z_comp", "visuosp_z_comp", 
     "memory_z_comp", "language_z_comp"];
+  const [feature, setFeature] = useState<string>(z_score_features[0]);
   const feature_list: string[] = ["insnpsi_age", "attent_z_comp", "npsid_ddur_v", "attent_sum_z"
     , "attent_sum"];
 
   let emptyPatient: Patient = new Patient();
   const [patients_data, setData] = useState(Array(54).fill(emptyPatient)); // todo, hard coded 54
-  console.log(patients_data);
-
   useEffect(() => {
     async function load() {
       let loaded = (await d3.csv("dataset/PD_SampleData_Curated.csv")).map((r) => Patient.fromJson(r));
@@ -329,8 +321,7 @@ function App() {
     }
     load();
   }, []);
-  console.log("after")
-  console.log(patients_data);
+
 
   return (
     <>
@@ -348,7 +339,11 @@ function App() {
       <select name="feature" id="feature" onChange={(e) => setFeature(e.target.value)}>
         {z_score_features.map((f) => <option value={f}>{f}</option>)}
       </select>
-      <PlotScatterplot feature={feature} patients_data={patients_data}/>
+      <div className="flex-container">
+        <PlotScatterplot y_feature={feature} x_feature='insnpsi_age' patients_data={patients_data}/>
+        <PlotScatterplot y_feature={feature} x_feature='npsid_ddur_v' patients_data={patients_data}/>
+      </div>
+      
     </>
   );
 }
