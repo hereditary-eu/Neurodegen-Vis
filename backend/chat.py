5b0251f6cd417ffb5b61ab9fe046b22c7efe3523
Benedikt Kantz <benedikt@kantz.at>
2025-11-24 10:09:31 +0100

    reduce timeout

--- backend/chat.py
+++ backend/chat.py
@@ -6,29 +6,29 @@ from pydantic import BaseModel
 # from dotenv import load_dotenv
 # load_dotenv()
 
-chat_router = APIRouter() # prefix="/chat", tags=["chat"]
+chat_router = APIRouter()  # prefix="/chat", tags=["chat"]
 
 if os.getenv("SERVER") == "true":
     client = OpenAI(
-      api_key=os.getenv("OPENAI_API_KEY"),
-      base_url="http://hereditary.cgv.tugraz.at/lm/api/v1"
+        api_key=os.getenv("OPENAI_API_KEY"),
+        base_url="http://hereditary.cgv.tugraz.at/lm/api/v1",
     )
-else: 
-    client = OpenAI(
-      api_key=os.getenv("OPENAI_API_KEY"))
+else:
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 class ChatRequest(BaseModel):
     messages: list
 
+
 MODEL = "gpt-4o-mini"
+
 
 @chat_router.post("/chat")
 async def chat(req: ChatRequest):
     # print("Received messages:", req.messages)
     completion = client.chat.completions.create(
-        model=MODEL,
-        messages=req.messages
+        model=MODEL, messages=req.messages, timeout=180
     )
     response = completion.choices[0].message.content
     print("Generated response:", response)
